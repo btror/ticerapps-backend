fa28e1a9c702f83bb3278fac6ebde86cd9dfbb0e
btror <ticer1999@gmail.com>
2023-07-25 21:27:56 -0400

    create github workflow and CI process, add mock data to tests

--- test_app.py
+++ test_app.py
@@ -1,15 +1,21 @@
 import unittest
 import requests
+from unittest.mock import patch
 
 
 class TestApp(unittest.TestCase):
     BASE_URL = "http://127.0.0.1:5000"
 
-    def test_send_message(self):
+    @patch("requests.post")
+    def test_send_message(self, mock_post):
         data = {
             "sender_email": "sender@example.com",
             "message_body": "test message",
         }
+        mock_response = mock_post.return_value
+        mock_response.status_code = 200
+        mock_response.text = "Message sent successfully"
+
         response = requests.post(f"{self.BASE_URL}/send_message", json=data)
 
         self.assertEqual(response.status_code, 200)
